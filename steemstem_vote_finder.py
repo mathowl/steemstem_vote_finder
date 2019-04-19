b19e946a95d3a6afa29da8371e18c9dbc62373a4
mathowl <45641277+mathowl@users.noreply.github.com>
2019-04-19 18:24:09 +0200

    Update steemstem_vote_finder.py

--- steemstem_vote_finder.py
+++ steemstem_vote_finder.py
@@ -4,7 +4,7 @@ from beem.instance import set_shared_steem_instance
 from beem.comment import Comment
 from unidecode import unidecode
 
-# outputs a .txt of steemstem voted and tagged links of a steem user. The .txt file is titled USER_steemstem where USER is the requested user. The .txt file is html coded and contains refs to the corresponding posts. 
+# outputs a .txt of steemstem voted and tagged links of a steem user. The .txt file is titled USER_steemstem where USER is the requested user. The .txt file is html coded and contains refs to the corresponding posts. It makes use of blog_history available which can only only retrieve posts up until a specific point
 
 def steemstem_vote_finder(user):
     stm=Steem(node=['https://rpc.steemviz.com', 'https://api.steem.house', 'https://gtg.steem.house:8090', 'wss://gtg.steem.house:8090', 'https://steemd-appbase.steemit.com', 'wss://steemd.privex.io', 'https://steemd.privex.io', 'wss://anyx.io', 'https://anyx.io', 'wss://rpc.curiesteem.com', 'https://rpc.buildteam.io', 'https://rpc.steemliberator.com', 'https://appbasetest.timcliff.com', 'wss://rpc.steemviz.com', 'https://steemd.minnowsupportproject.org'])
